1be8b6b191fb2a6f2e1b0d624d0085c47fb090a1
j0h3 <jonathandautricourt@gmail.com>
2021-01-17 15:04:07 +0100

    Modify EC2 terraform

--- ec2/variable.tf
+++ ec2/variable.tf
@@ -9,16 +9,6 @@ variable "region" {
   default = "eu-west-3"
 }
 
-variable "access_key" {
-  type = string
-  description = "AWS access key"
-}
-
-variable "secret_key" {
-  type = string
-  description = "AWS secret key"
-}
-
 variable "instance_type" {
   type = string
   description = "Instance type"
